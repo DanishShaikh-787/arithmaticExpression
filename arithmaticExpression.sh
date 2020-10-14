d47278d6763557f2b63f60371e7c2b94efccb01d
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-14 15:53:35 +0530

    Added : Result Store in Dictonary

--- arithmaticExpression.sh
+++ arithmaticExpression.sh
@@ -4,12 +4,11 @@ read -p "Value a : " a
 read -p "Value b : " b
 read -p "Value c : " c
 
-value1=$(echo "scale=2; $a+$b*$c " |bc )
-value2=$(echo "scale=2; $a*$b+$c " |bc )
-value3=$(echo "scale=2; $c+$a/$b " |bc )
-value4=$(echo "scale=2; $a%$b+$c " |bc )
+declare -A result
+result[value1]=$(echo "scale=2; $a+$b*$c " |bc )
+result[value2]=$(echo "scale=2; $a*$b+$c " |bc )
+result[value3]=$(echo "scale=2; $c+$a/$b " |bc )
+result[value4]=$(echo "scale=2; $a%$b+$c " |bc )
 
-echo "Compute (a+b*c) : " $value1
-echo "Compute (a*b+c) : " $value2
-echo "Compute (c+a/b) : " $value3
-echo "Compute (a%b+c) : " $value4
+
+echo "Dict " ${result[@]}
