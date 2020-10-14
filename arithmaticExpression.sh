13134ee3c3b5bbe1bc670bb8e58f4006ff75ebd4
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-14 16:04:56 +0530

    Added : Result Sorted IN Descending Order

--- arithmaticExpression.sh
+++ arithmaticExpression.sh
@@ -5,16 +5,39 @@ read -p "Value b : " b
 read -p "Value c : " c
 
 declare -A result
-result[value1]=$(echo "scale=2; $a+$b*$c " |bc )
-result[value2]=$(echo "scale=2; $a*$b+$c " |bc )
-result[value3]=$(echo "scale=2; $c+$a/$b " |bc )
-result[value4]=$(echo "scale=2; $a%$b+$c " |bc )
+result[value1]=$(($a+$b*$c))
+result[value2]=$(($a*$b+$c))
+result[value3]=$(($c+$a/$b))
+result[value4]=$(($a%$b+$c))
+
 
 for value in ${result[@]}
 do
 	echo  $value
-	array[j]=$value
-	((j++))
+	array[i]=$value
+	((i++))
+
 done
 
 echo "array" ${array[@]}
+
+#decending Array
+
+for ((i = 0; i<5; i++))
+do
+    for((j = 0; j<5-i-1; j++))
+    do
+        echo "$j"
+	if [ ${array[j]} -lt ${array[$((j+1))]} ]
+        then
+            	# swap
+            	temp=${array[j]}
+            	array[$j]=${array[$((j+1))]}
+            	array[$((j+1))]=$temp
+		echo ${array[@]}
+        fi
+    done
+done
+
+echo "Array in sorted in Descending order :"
+echo ${array[@]}
