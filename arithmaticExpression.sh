0241de06ff7a16f023ca000bf587ade0b90398e2
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-14 15:30:10 +0530

    Added : Read Three Value From User

--- arithmaticExpression.sh
+++ arithmaticExpression.sh
@@ -1,3 +1,9 @@
 #! /bin/bash -x
 
-echo "Welcome To Arithmatic  Expressions "
+read -p "Value 1 : " value1
+read -p "Value 2 : " value2
+read -p "Value 3 : " value3
+
+echo "Value 1 : " $value1
+echo "Value 2 : " $value2
+echo "Value 3 : " $value3
