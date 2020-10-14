67aeb06ddb04bd5ebea054ddf428f4756a0434a1
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-14 15:49:07 +0530

    Added : Compute a%b+c Value

--- arithmaticExpression.sh
+++ arithmaticExpression.sh
@@ -7,7 +7,9 @@ read -p "Value c : " c
 value1=$(echo "scale=2; $a+$b*$c " |bc )
 value2=$(echo "scale=2; $a*$b+$c " |bc )
 value3=$(echo "scale=2; $c+$a/$b " |bc )
+value4=$(echo "scale=2; $a%$b+$c " |bc )
 
 echo "Compute (a+b*c) : " $value1
 echo "Compute (a*b+c) : " $value2
 echo "Compute (c+a/b) : " $value3
+echo "Compute (a%b+c) : " $value4
